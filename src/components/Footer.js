291a6c6c942465febd230308652384769703a52a
Lantao home PC <lantaook@gmail.com>
2023-03-07 00:44:13 -0500

    fixed anchor open in a new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,19 @@ export default function Footer() {
     <div>
       <div className="flex justify-center mt-10  h-10 ">
         {/* <img src={github} alt="github logo" /> */}
-        <a href="https://github.com/pppzlt" className="inline-block h-10">
+        <a
+          href="https://github.com/pppzlt"
+          className="inline-block h-10"
+          rel="noreferrer"
+          target="_blank"
+        >
           <img src={githubwhite} alt="github logo" className="mr-5 h-10" />
         </a>
         <a
           href="https://www.linkedin.com/in/lantao-zhang"
           className="inline-block h-10"
+          rel="noreferrer"
+          target="_blank"
         >
           <img src={linkedin} alt="linkedin logo" className="h-10" />
         </a>
