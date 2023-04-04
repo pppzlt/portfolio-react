5943af23aeb11a8d7c10c85dc0490249f1e78016
Lantao home PC <lantaook@gmail.com>
2023-04-04 14:13:39 -0400

    change icon and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import './App.css';
+import { useEffect } from 'react';
+
+
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Lantao Zhang"
+  })
   return (
     <div >
       <Navigation  />
